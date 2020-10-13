f2d60cd8a856ba6b188d0c0d605722a40b38471b
Sergo048rus <sergo048rus@yandex.ru>
2020-10-14 02:16:11 +0300

    GUI мать ее

--- test.py
+++ test.py
@@ -1,12 +1,13 @@
 from PyQt5 import QtGui
-from PyQt5.QtWidgets import QApplication, QMainWindow, QGraphicsScene, QGraphicsView, QSizePolicy
+# from PyQt5.QtGui import QFont
+from PyQt5.QtWidgets import QApplication, QMainWindow, QGraphicsScene, QGraphicsView, QSizePolicy, QPushButton, QLineEdit,QFileDialog,QTextEdit
 
 import sys
 from Hanoi import *
 
 from PyQt5.QtGui import QBrush, QPen
 from PyQt5.QtCore import Qt
-import InputTXT as parser
+import InputTXT as par
 
 
 
@@ -18,15 +19,17 @@ class Window(QMainWindow):
         self.top = 100
         self.left = 100
         self.width = 1800
-        self.heigth = 600
+        self.heigth = 800
         
         self.Hanoi = Hanoi
+        self.parser = par.InputTXT()
+        self.InitWindow()
 
         
     
     def InitWindow(self):
 
-        scene = QGraphicsScene()
+        self.scene = QGraphicsScene()
         
         redBrush = QBrush(Qt.red)
         blueBrush = QBrush(Qt.blue)
@@ -37,16 +40,50 @@ class Window(QMainWindow):
         bluePen = QPen(Qt.blue)
         bluePen.setWidth(2)
 
-        view = QGraphicsView(scene,self)
+        view = QGraphicsView(self.scene,self)
         view.setGeometry(0,0,1800,500)
         # view.setSizePolicy(QSizePolicy.Expanding, QSizePolicy.Fixed)
         
         self.setWindowIcon(QtGui.QIcon("doc/apelsin.png"))
         self.setWindowTitle(self.title)
         self.setGeometry(self.top, self.left, self.width, self.heigth)
-        self.show()
+        
        
-        return scene
+        #-----------------------------------------------------------------------------------------------------------
+        #Определяем кнопки
+
+        btn1 = QPushButton("Start order", self)
+        btn1.setGeometry(30, 510,100,30)
+
+        btn2 = QPushButton("Start optimization", self)
+        btn2.setGeometry(150, 510,100,30)
+
+        btn3 = QPushButton("Step back", self)
+        btn3.setGeometry(150, 545,100,30)
+
+        btn4 = QPushButton("Step forward", self)
+        btn4.setGeometry(30, 545,100,30)
+
+        btnFile = QPushButton("File...", self)
+        btnFile.setGeometry(605, 510,100,30)
+
+
+        btn1.clicked.connect(self.startOrderClicked)
+        btn2.clicked.connect(self.startOptimClicked)
+        btn3.clicked.connect(self.stepOrderBack)
+        btn4.clicked.connect(self.stepOrderForward)
+        btnFile.clicked.connect(self.getFile)
+        #-----------------------------------------------------------------------------------------------------------
+        #Определяем lineEdit
+        self.qle = QLineEdit(self)
+        # qle.move(300, 510)
+        self.qle.setGeometry(300, 511,300,28)
+        #-----------------------------------------------------------------------------------------------------------
+        #Типа консоль
+        self.Console = QTextEdit(self)
+        self.Console.setGeometry(720, 510,500,250)
+        self.show()
+        
         # rect2 = Hanoi.disk_place(0,2,20,scene)
 
         # rect2 = Hanoi.disk_place(0,1,2,scene)
@@ -56,7 +93,30 @@ class Window(QMainWindow):
         # rect5 = Hanoi.disk_place(10,20,1,scene)
         # scene.removeItem(rect1)
         #disc_list[7] = Hanoi.disk_place(2,0,7,scene)
+    
+    def startOrderClicked(self):
+        numDisk, numColumn, Cost, err = self.parser.ReadDisk(self.filename)
+    
+        if err == 'OK':
+            disc_list = Hanoi.StartInit(numDisk,numColumn,self.scene)
 
+        order, sizeOrder, returnErr = self.parser.ReadOrder(self.filename)
+
+    def startOptimClicked(self):
+        pass
+
+    def stepOrderForward(self):
+        pass
+
+    def stepOrderBack(self):
+        pass
+
+    def getFile(self):                                                     
+        self.filename, self.filetype = QFileDialog.getOpenFileName(self,
+                             "Выбрать файл",
+                             ".",
+                             "Text Files(*.txt);;All Files(*)")
+        self.qle.setText((self.filename))
 
 
 if __name__ == "__main__": 
@@ -64,25 +124,15 @@ if __name__ == "__main__":
     App = QApplication(sys.argv)
     window = Window(Hanoi)
 
-    parser = parser.InputTXT()
-    FILENAME = "tests_file/t3-3.txt" 
-
-    numDisk, numColumn, Cost, err = parser.ReadDisk(FILENAME)
+   
     
-    scene = window.InitWindow()
 
     
-           
-
-    if err == 0:
-        disc_list = Hanoi.StartInit(numDisk,numColumn,scene)
-
-    order, sizeOrder, returnErr = parser.ReadOrder(FILENAME)
     
     
-    if returnErr == 0 and err == 0:
-        for i in range(0,sizeOrder):
-            scene.removeItem(disc_list[order[i]['dI']])
+    # if returnErr == 0 and err == 0:
+    #     for i in range(0,sizeOrder):
+    #         scene.removeItem(disc_list[order[i]['dI']])
                 
             
 
