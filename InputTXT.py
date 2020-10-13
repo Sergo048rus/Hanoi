7e5132083b242660cdcede09b0841ed1bd15005b
Sergo048rus <sergo048rus@yandex.ru>
2020-10-13 23:48:30 +0300

    КАРоч оно работает с Test1 должно хавать все, я про парсер

--- InputTXT.py
+++ InputTXT.py
@@ -6,7 +6,7 @@ CONST_ORDER  = CONST_COST + 4
 
 
 class InputTXT():
-    def __init__(self) -> None:
+    def __init__(self):
     
         self.numColumn = 0
         self.numDisk = 0
@@ -15,7 +15,63 @@ class InputTXT():
         
         self.order = []
 
+    def __Check_NPARTS(self,l):
+        self.line = 0
+        try:
+            while l[self.line].split()[0] != 'NPARTS':
+                self.line = self.line + 1
+
+            self.buff = ''   
+            for i in range(0,len(l[self.line].split()[1])):
+                self.buff = self.buff + l[self.line].split()[1][i]
+            try:
+                self.numDisk = int(self.buff)                       # Определение количества дисков
+            except ValueError:
+                self.returnErr = 'Init: The disk is not a number'
+
+            self.buff = ''                                          # Обнуляем буфер
+
+            if self.numDisk > 20 or self.numDisk <= 0:
+                self.returnErr = 'Init: Disk greater than the maximum or less than zero'
+
+            self.line = self.line + 1
+
+        except IndexError:
+             self.returnErr = 'Err file!!!!!!!2'
+        
+        self.__Check_COST(l)                    # Вызываем следующий этап проверки
     
+    def __Check_COST(self,l):                               
+        try:
+            while l[self.line] != 'COST':             
+                self.line = self.line + 1
+
+            self.buff = ''                                 
+            for i in range(len(l[self.line + 1])):                   # Определение цены штырей и их количества
+                if l[self.line + 1][i] == ' ':
+                    self.numColumn = self.numColumn + 1
+                    try:
+                        if int(self.buff) > 0:
+                            self.diskCost.append(int(self.buff))
+                        else:
+                            self.diskCost.append(int(self.buff))
+                            self.returnErr = 'Init: Rod greater than the maximum or less than zero'
+                    except ValueError:
+                        self.returnErr = 'Init: The rod is not a number'
+                    self.buff = ''
+                else:
+                    self.buff = self.buff + l[self.line + 1][i]
+            self.numColumn = self.numColumn + 1                      # Добавляем последнее значение
+            try:
+                self.diskCost.append(int(self.buff))
+            except ValueError:
+                self.returnErr = 'Init: The rod is not a number'
+
+            self.line = self.line + 1
+        except IndexError:
+            self.returnErr = 'Err file!!!!!!!4'
+            
+
     def ReadDisk(self,fileName) -> int:
         self.returnErr = 'OK'
         try:
@@ -25,52 +81,62 @@ class InputTXT():
         if self.returnErr == 'OK':
             l = [line.strip() for line in self.f]
             self.f.close()
-            # print(l)
-            nparts = ''
-            for i in range(0,6):
-                nparts = nparts + l[CONST_NPARTS][i]
-            if nparts == 'NPARTS':  
-                self.buff = ''          
-                for i in range(7,len(l[CONST_NPARTS])):
-                    self.buff = self.buff + l[CONST_NPARTS][i]
-                try:
-                    self.numDisk = int(self.buff)                       # Определение количества дисков
-                except ValueError:
-                    self.returnErr = 'Init: The disk is not a number'
-
-                self.buff = ''                                          # Обнуляем буфер
-
-                if self.numDisk > 20 or self.numDisk <= 0:
-                    self.returnErr = 'Init: Disk greater than the maximum or less than zero'
-            else:
-                 self.returnErr = 'Err file!!!!!!!'
-            if l[CONST_COST] == 'COST':  
-                self.buff = ''                                 
-                for i in range(len(l[CONST_COLUMN])):                   # Определение цены штырей и их количества
-                    if l[CONST_COLUMN][i] == ' ':
-                        self.numColumn = self.numColumn + 1
-                        try:
-                            if int(self.buff) > 0:
-                                self.diskCost.append(int(self.buff))
-                            else:
-                                self.diskCost.append(int(self.buff))
-                                self.returnErr = 'Init: Rod greater than the maximum or less than zero'
-                        except ValueError:
-                             self.returnErr = 'Init: The rod is not a number'
-                        self.buff = ''
-                    else:
-                        self.buff = self.buff + l[CONST_COLUMN][i]
-                self.numColumn = self.numColumn + 1                      # Добавляем последнее значение
-                try:
-                    self.diskCost.append(int(self.buff))
-                except ValueError:
-                    self.returnErr = 'Init: The rod is not a number'
-
-            else:
-                 self.returnErr = 'Err file!!!!!!!'
-
+            
+            while l.count('') != 0:
+                l.remove('')
+ 
+            self.__Check_NPARTS(l)
+            
             return int(self.numDisk),int(self.numColumn),self.diskCost,self.returnErr
         
+
+    def __checkOrder(self,l):
+        while l[self.line] != 'ORDER':             
+                self.line = self.line + 1
+        self.line = self.line + 1
+        startOrderLine = self.line
+
+        while l[self.line][0] != '/':
+           
+            buffOrder = {}
+            self.buff = ''  
+            count = 1                               
+            for i in range(len(l[self.line])):                   # Определение цены штырей и их количества
+                if l[self.line][i] == ' ' and count == 1:
+                    try:
+                        buffOrder['dI'] = int(self.buff)
+                        if buffOrder['dI'] > self.numDisk - 1 or buffOrder['dI'] < 0:
+                            self.returnErr = 'Order: Disk greater than the maximum or less than zero'
+                    except ValueError:
+                        self.returnErr = 'Order: The disk is not a number'
+                    self.buff = ''
+                    count = count + 1
+                else: 
+                    if l[self.line][i] == ' ' and count == 2:
+                        try:
+                            buffOrder['rSrc'] = int(self.buff)
+                            if buffOrder['rSrc'] > self.numColumn - 1 or buffOrder['rSrc'] < 0:
+                                self.returnErr = 'Order: Rod greater than the maximum or less than zero'
+                        except ValueError:
+                            self.returnErr = 'Order: The rod is not a number'
+                        self.buff = ''
+                        count = 1
+                    else:
+                        self.buff = self.buff + l[self.line][i]
+
+            try:
+                buffOrder['rDst'] = int(self.buff)
+                if buffOrder['rDst'] > self.numColumn - 1 or buffOrder['rDst'] < 0:
+                    self.returnErr = 'Order: Rod greater than the maximum or less than zero'
+                self.order.append(buffOrder)
+            except ValueError:
+                self.returnErr = 'Order: The rod is not a number'  
+
+            self.line = self.line + 1                           # Переходим на следующую линию
+        self.sizeOrder = self.line - startOrderLine
+    
+
+
     def ReadOrder(self,fileName):
         self.returnErr = 'OK'
         try:
@@ -81,54 +147,18 @@ class InputTXT():
             l = [line.strip() for line in self.f]
             self.f.close()
             # print(l)
+            while l.count('') != 0:
+                l.remove('')
 
-            if l[CONST_ORDER] == 'ORDER':  
-                self.buff = ''
-                lineOrder = CONST_ORDER + 1                             # Стартуем со следующей строки
-                while l[lineOrder][0] != '/':
-                    # buffOrder = listOrder()
-                    buffOrder = {}
-                    self.buff = ''  
-                    count = 1                               
-                    for i in range(len(l[lineOrder])):                   # Определение цены штырей и их количества
-                        if l[lineOrder][i] == ' ' and count == 1:
-                            try:
-                                buffOrder['dI'] = int(self.buff)
-                                if buffOrder['dI'] > self.numDisk - 1 or buffOrder['dI'] < 0:
-                                    self.returnErr = 'Order: Disk greater than the maximum or less than zero'
-                            except ValueError:
-                                self.returnErr = 'Order: The disk is not a number'
-                            self.buff = ''
-                            count = count + 1
-                        else: 
-                            if l[lineOrder][i] == ' ' and count == 2:
-                                try:
-                                    buffOrder['rSrc'] = int(self.buff)
-                                    if buffOrder['rSrc'] > self.numColumn - 1 or buffOrder['rSrc'] < 0:
-                                        self.returnErr = 'Order: Rod greater than the maximum or less than zero'
-                                except ValueError:
-                                    self.returnErr = 'Order: The rod is not a number'
-                                self.buff = ''
-                                count = 1
-                            else:
-                                self.buff = self.buff + l[lineOrder][i]
-
-                    try:
-                        buffOrder['rDst'] = int(self.buff)
-                        if buffOrder['rDst'] > self.numColumn - 1 or buffOrder['rDst'] < 0:
-                            self.returnErr = 'Order: Rod greater than the maximum or less than zero'
-                        self.order.append(buffOrder)
-                    except ValueError:
-                        self.returnErr = 'Order: The rod is not a number'  
-
-                    lineOrder = lineOrder + 1                           # Переходим на следующую линию
-                self.sizeOrder = lineOrder - CONST_ORDER-1
+            self.__checkOrder(l)
+ 
         return self.order, self.sizeOrder, self.returnErr
 
   
 
 
 if __name__ == "__main__":  
+
     test = InputTXT()
 
     FILENAME = "tests_file/Test1.txt" 
@@ -136,6 +166,7 @@ if __name__ == "__main__":
     print(' Disk = ',disk,'\n','Column = ', column,'\n','Err = ', err,'\n', 'Cost', diskCost)
 
     order,sizeOrder, returnErr = test.ReadOrder(FILENAME)
+    print(len(order), sizeOrder)
     print('Err = ', returnErr)
     for i in range(0, sizeOrder):
         print(' disk  = ',order[i]['dI'], 'columnFrom = ',order[i]['rSrc'], 'columnTo = ',order[i]['rDst'],'\n')
