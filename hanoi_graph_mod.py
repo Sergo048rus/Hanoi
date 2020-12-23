7e11cf2fe325fcd4542a678fc1f05427846f7db9
Vladislav Kochetkov <vakochetkov3391@gmail.com>
2020-12-23 16:49:18 +0300

    hanoi_graph_mod

--- hanoi_graph_mod.py
+++ hanoi_graph_mod.py
@@ -15,22 +15,142 @@ from stopwatch import StopWatch
 def BuildPath(diskCount, rodList, src, dst):
     graph = nx.DiGraph() 
     root = str(str(src) * diskCount) # строка, потом хешируется
-    graph.add_node(root, weight=self.rodList[0], name=root) 
+    graph.add_node(root, weight=rodList[0], name=root) 
 
     genCounter = 0
     isEmpty = False
-    while(isEmpty):
-        isEmpty = makeGen(graph, src,dst)
+    placedCount = 0
+    prevGen = [root]
+
+    while not isEmpty:
+        prevGen, isEmpty, placedCount = makeGen(graph, prevGen, placedCount, src, dst, diskCount, rodList)
         genCounter += 1
-        print("mkGen #{0}".format(genCounter))
-        
+        print("mkGen #{0} {1}".format(genCounter, len(prevGen)))
+
     print("Solved!")
 
-def makeGen(graph, src, dst):
+    return calcCostBF(graph, src, dst, diskCount)
 
 
+def makeGen(graph, prevGen, placedCount, src, dst, diskCount, rodList):
+    newGen = [] # TODO: should be optimized and deleted
+    rodRange = range(0,len(rodList))
+    diskPlaced = False
+        
+    # для каждой ноды в текущем поколении
+    for node in prevGen:
+        srcBlocked = False
+        dstBlocked = False
+
+        # для каждого диска в ноде берем все возможные/невозможные варианты
+        for i in range(0,len(node) - placedCount): # без уже поставленных на место дисков
+            if i != 0:
+                for j in range(0,i):
+                    if node[j] == node[i]:
+                        srcBlocked = True
+                        break
+                if srcBlocked:
+                    srcBlocked = False
+                    continue
+        
+            for r in rodRange:
+                if r != int(node[i]):        # если возможна замена, меняем i-й элемент 
+                    if i == (len(node) - 1): # !check boundary cases!
+                        repl = node[:i] + str(r)
+                        for k in range(0,i):
+                            if node[k] == str(r):
+                                dstBlocked = True
+                                break
+                    elif i == 0:
+                        repl = str(r) + node[1:]
+                    else:
+                        repl = node[:i] + str(r) + node[i+1:] 
+                        for k in range(0,i):
+                            if node[k] == str(r):
+                                dstBlocked = True
+                                break
+                     
+
+                    if dstBlocked:
+                        dstBlocked = False
+                        continue
+                       
+                    # если такая нода есть - не добавляем
+                    if graph.has_node(repl):
+                        continue
+                    
+                    # иначе добавляем
+                    # print(node + "->" + repl + " i: " + str(int(node[i])))
+                    graph.add_node(repl, weight=rodList[r], name=repl) 
+                    graph.add_edge(node, repl, weight=rodList[r])
+
+                    newGen.append(repl) 
+                    if repl[-1 - placedCount] == str(dst) and repl[-1 - placedCount + 1] == str(dst):
+                        # print(repl + ' = ' + targetRode)
+                        diskPlaced = True
+                                
+    if diskPlaced:  
+        placedCount += 1
+        # раз диск поставлен надо удалить остальные "неправильные" ноды
+        newGen = [x for x in newGen if str(str(dst) * placedCount) in x[-1 - placedCount + 1:]] 
+    
+    if len(newGen) != 0:
+        isEmpty = False
+    else:
+        isEmpty = True
+
+    return newGen, isEmpty, placedCount
+
+
+#############################################
+# Pathfinders
+#############################################
+def calcCostDeijkstra(graph, src, dst, diskCount):
+        print("Start pathfinder - Deijkstra")
+        srcNode = str(src) * diskCount
+        dstNode = str(dst) * diskCount
+        print(srcNode + "-D>" + dstNode)
+        return nx.single_source_dijkstra(graph, srcNode, dstNode)
+
+def calcCostBF(graph, src, dst, diskCount):
+        print("Start pathfinder - Bellman-Ford")
+        srcNode = str(src) * diskCount
+        dstNode = str(dst) * diskCount
+        print(srcNode + "-D>" + dstNode)
+        return nx.single_source_bellman_ford(graph, srcNode, dstNode)
+
+#############################################
+# Вывод в файл
+#############################################
+def exportPathToFile(filepath, path, cost):
+    with open(str(filepath), "w", encoding="utf-8") as fos:
+        fos.write("=== Hanoi pathfinder result ===\r\n")
+        fos.write("Cost: {0}\r\n".format(cost))
+        fos.write("Path:\r\n")
+        for move in path:
+            fos.write("{0}\n".format(move))
+
+def exportPathToFileAlternate(filepath, path, cost):
+    with open(str(filepath), "w", encoding="utf-8") as fos:
+        fos.write("=== Hanoi pathfinder result ===\r\n")
+        fos.write("Cost: {0}\r\n".format(cost))
+        fos.write("Path:\r\n")
+        for i in range(1,len(path)):
+            disk,src,dst = "","",""
+            for j in range(0,len(path[i])):
+                if path[i][j] != path[i-1][j]:
+                    disk = j
+                    src = path[i-1][j]
+                    dst = path[i][j]
+                    break
+            fos.write("{0} {1} {2}\n".format(disk, src, dst))
+
+
+#############################################
+# MAIN
+#############################################
 if __name__ == "__main__": 
-    FILENAME = "10d3r.txt"
+    FILENAME = "6d4r.txt"
 
     if len(sys.argv) < 2:
         print("WARN! Use predefined value!")   
@@ -53,16 +173,16 @@ if __name__ == "__main__":
 
     if err == 'OK':
         sw = StopWatch()
-        cost, path = BuildPath(diskCount, diskCost, debugLevel=0)
+        cost, path = BuildPath(diskCount, diskCost, 0, 1)
         sw.stop()
-        
+
+        print(cost, path)
+        # sys.exit(0)
+
         # exportPath = os.path.abspath(os.path.dirname(os.path.abspath(__file__)) + ("/solver_out/{0}_out.txt".format(FILENAME.split('.')[0])))
         # graph.exportGen(exportPath)
         # graph.draw()
 
-        # sw = StopWatch()
-        # calcCostBF(0,1) 
-        # sw.stop()
 
         # один формат наш, другой Васекина, можно оставить любой или выпилить все для скорости
         exportPath1 = os.path.abspath(os.path.dirname(os.path.abspath(__file__)) + ("/solver_out/{0}_path.txt".format(FILENAME.split('.')[0])))
